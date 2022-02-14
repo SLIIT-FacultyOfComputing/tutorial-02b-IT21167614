e3862bb11f62c0c3b9ca5528a89083e720c91504
D.N.Mahagedara <99257476+IT21167614@users.noreply.github.com>
2022-02-14 04:18:42 +0000

    Converted To C++

--- tute04.cpp
+++ tute04.cpp
@@ -15,6 +15,9 @@ Do not modify the main function.*/
 
 long Factorial(int no);
 long nCr(int n, int r);
+int a=1; //n!
+int b =1; //r!
+int c = 1; //(n-r)!
 
 int main() {
   int n, r;
@@ -26,4 +29,19 @@ int main() {
   std::cout << nCr(n,r);
   std::cout << std::endl;
   return 0;
+}
+long Facorial(int no){
+  long ans = 1;
+  for(int i = 1; i <= no; i++){
+    ans = ans * i;
+  }
+  return ans;
+}
+long nCr(int n, int r){
+  long ans;
+  a = Facorial(n);
+  b = Facorial(r);
+  c = Facorial(n-r);
+  ans = a/(b*c);
+  return ans;
 }
